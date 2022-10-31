08ba74699a94232c7721d2e3e03c677c775243cc
vackoma21 <vackoma21@sps-prosek.cz>
2022-10-31 11:11:36 +0100

    change

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 contn = True
+print("Welcome to the Calculator")
 while contn:
-    print ("Welcome to the Calculator")
-    num1 = float (input("Please write the first number: "))
+    correctNum1 = True
+    while correctNum1:
+        correctNum1 = False
+        num1 = float(input("Please write the first number: "))
+
+    while
     num2 = float (input("Please write the second number: "))
     contn2 = True
-    operation = input("Choose an operation: 1 is for +, 2 is for -, 3 is for *, 4 is for /: ")
 
     while contn2:
         contn2 = False
+        operation = input("Choose an operation: 1 is for +, 2 is for -, 3 is for *, 4 is for /: ")
         if operation == "1":
             result = num1 + num2
         elif operation == "2":
@@ -24,4 +29,16 @@ while contn:
             contn2 = True
 
     print(result)
+    notAdded = True
+    while notAdded:
+        again = input("Do you want to continue? A / N: ")
+        if again.lower() == "a":
+            contn = True
+            notAdded = False
+        elif again.lower() == "n":
+            contn = False
+            notAdded = False
+        else:
+            notAdded = True
+
 
