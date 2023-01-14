b326921615af196b4cdce6a0a2b815e8a2031988
DarvilleraD <vackoma21@sps-prosek.cz>
2023-01-14 22:31:45 +0100

    pls

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,67 @@
-cards = {
-    '2': 'hearts',
-    '': ''
-}
+import random
+import os
 
 deck = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A']
 signs = ['H', 'D', 'C', 'S']
+
+minPlayers = 2
+maxPlayers = 7
 # make deck with list comprehension
-deck = [{'value': value, 'sign': sign}]
+# deck = [i for i in {'value': values, 'sign': signs}]
 
 print(deck)
+
+# for value in values:
+#     for sign in signs:
+#         deck.append({'value': value, 'sign': sign})
+#
+# # print(deck)
+# for x in range(len(deck)):
+#     print(deck[x])
+
+deck = [[{'value': value, 'sign': sign}] for value in values for sign in signs]
+for x in range(len(deck)):
+    print(deck[x])
+
+newRound = True
+while newRound:
+    playerNo = input('How many people are playing? : ')
+
+    cardsInPlay = []
+
+    # Number of players is between the MAX and MIN values
+    while playerNo.isnumeric() and maxPlayers >= int(playerNo) >= minPlayers:
+
+        # rand = random.randint(0, len(deck))
+        # print(rand)
+
+        # deck.pop(rand)
+        # for x in range(len(deck)):
+        #     print(deck[x])
+
+        # if there is less then 2 cards left, error, please fix !!
+        for x in range(2):
+            print('Lenght01:', len(deck))
+            for i in range(int(playerNo)):
+                maxDeckIndex = len(deck)-1
+                randCard = random.randint(0, maxDeckIndex)
+                print('Lenght:', len(deck))
+                print('Pop:', randCard)
+                print(deck[randCard])
+                cardsInPlay.append([i, deck[randCard]])
+                deck.pop(randCard)
+                print(randCard)
+            print(cardsInPlay)
+
+        for card in cardsInPlay:
+            print(card)
+            for index in card:
+                print('INDEX: ', index)
+
+        # for x in range(len(deck)):
+        #     print(deck[x])
+        # break
+
+
 
--- test.py
+++ test.py
@@ -4,5 +4,9 @@ list2 = [2,8,0]
 ratio = (len(list2)/len(list1))*100
 print(round(ratio))
 
-for i in list1:
-    print(i)
+# for i in list1:
+#     print(i)
+print(len(list1))
+list1.pop(0)
+print(list1[0])
+print(len(list1))
