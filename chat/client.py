6ddfdd2b853ab3164456de7e47bbae2856ec752f
DarvilleraD <vackoma21@sps-prosek.cz>
2023-06-20 21:46:01 +0200

    pls

--- chat/client.py
+++ chat/client.py
@@ -1,15 +1,47 @@
 import threading
-from tkinter import *
+import tkinter as tk
 import socket
 
 server = socket.gethostbyname(socket.gethostname())
 
-#host = socket.gethostname()
-#host = '127.0.0.1'
 host = socket.gethostname()
 port = 2206
 user = []
 
+root = tk.Tk()
+root.title("Chat")
+root.geometry("800x500")
+
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+
+# frames
+chatFrame = tk.Frame(root, width=800, height=250)
+chatFrame.grid(row=0, column=0, padx=10, pady=5)
+
+usernameFrame = tk.Frame(chatFrame, bg="red", width=800)
+usernameFrame.grid(row=0, column=0, pady=5, sticky="nsew", columnspan=3)
+
+#chatScroll.config(command=chat.yview)
+
+#chatScroll = Scrollbar(root, orient="vertical")
+username = tk.Entry(usernameFrame)
+usernameLabel = tk.Label(usernameFrame, text="Username: ")
+sendUsername = tk.Button(usernameFrame, text="login")
+chat = tk.Text(chatFrame, fg="green", borderwidth=1)
+textLine = tk.Text(chatFrame, height=1, width=50, fg="green", borderwidth=1)
+
+
+chat.grid(row=1, column=0, columnspan=3)
+textLine.grid(row=2, column=0, columnspan=3, sticky="we")
+username.grid(row=0, column=1, sticky="nsew")
+usernameLabel.grid(row=0, column=0, sticky="nsew")
+sendUsername.grid(row=0, column=2, sticky="nsew")
 
 def send_to_server(conn):
     username = input('Username: ')
@@ -46,6 +78,8 @@ def get_message(conn):
 
 
 def clientCode():
+    root.mainloop()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
